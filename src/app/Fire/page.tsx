fdd4f116ef44a6a1c97bc5922688d1717a11b532
LaurisV22 <100121715+LaurisV22@users.noreply.github.com>
2023-11-28 08:45:56 +0000

    % slider added

--- src/app/Fire/page.tsx
+++ src/app/Fire/page.tsx
@@ -2,6 +2,13 @@
 import React, { useState } from 'react';
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 
 const FirePage = () => {
 // States for all the Input fields
@@ -14,55 +21,48 @@ const FirePage = () => {
     const [grossMonthlyIncome, setGrossMonthlyIncome] = useState('');
     const [monthlySavings, setMonthlySavings] = useState('');
     const [inflation, setInflation] = useState('');
-    const [earlyRetirementMethod, setEarlyRetirementMethod] = useState('25-rule');
+    const [retirementPercentage, setRetirementPercentage] = useState(50); // Slider state
+
 
     // Function to handle form submission
     const handleSubmit = (event) => {
         event.preventDefault();
         // Here you would handle the calculation and potentially send data to an API
     };
-
     return (
-        <div className="container my-8">
-            <form onSubmit={handleSubmit}>
-                <div className="form-row">
+        <div className="container  md:w-1/3 my-8">
+            <form className="flex flex-col gap-3"  onSubmit={handleSubmit}>
                     <label>Age<Input type="number" value={age} onChange={(e) => setAge(e.target.value)} /></label>
                     <label>Your retirement Age <Input type="number" value={retirementAge} onChange={(e) => setRetirementAge(e.target.value)} /></label>
-                </div>
-                <div className="form-row">
                     <label>Portfolio worth<Input step="1000" type="number" value={portfolioWorth} onChange={(e) => setPortfolioWorth(e.target.value)} /></label>
                     <label>Gross monthly income<Input step="100" type="number" value={grossMonthlyIncome} onChange={(e) => setGrossMonthlyIncome(e.target.value)} /></label>
-                </div>
-                <div className="form-row">
                     <label>Net monthly income<Input type="number" value={netMonthlyIncome} onChange={(e) => setNetMonthlyIncome(e.target.value)} /></label>
                     <label>Monthly savings<Input type="number" value={monthlySavings} onChange={(e) => setMonthlySavings(e.target.value)} /></label>
-                </div>
-                <div className="form-row">
                     <label>Interest<Input type="number" step="0.01" value={interest} onChange={(e) => setInterest(e.target.value)} /></label>
                     <label>Inflation<Input type="number" step="0.01" value={inflation} onChange={(e) => setInflation(e.target.value)} /></label>
-                </div>
-                <div className="form-row">
                     <label>Capital Gains Tax<Input type="number" step="0.01" value={capitalGainsTax} onChange={(e) => setCapitalGainsTax(e.target.value)} /></label>
-                    <label>Early retirement percentage (will be gained from portfolio)
-                        <select value={earlyRetirementMethod} onChange={(e) => setEarlyRetirementMethod(e.target.value)}>
-                            <option value="10%">10% </option>
-                            <option value="20%">20% </option>
-                            <option value="30%">30% </option>
-                            <option value="40%">40% </option>
-                            <option value="50%">50% </option>
-                            <option value="60%">60% </option>
-                            <option value="70%">70% </option>
-                            <option value="80%">80% </option>
-                            <option value="90%">90% </option>
-                            <option value="100%">100% </option>
+                <label>Early retirement percentage
+                    (will be gained from portfolio)
+                    {/* Slider input for retirement percentage */}
+                    <label className="block flex flex-col  my-3 ">
+                        <input
+                            type="range"
+                            className=" h-15 w-50 p-0 focus:outline-none focus:ring-0 focus:shadow-none"
+                            min="0"
+                            max="100"
+                            value={retirementPercentage}
+                            onChange={(e) => setRetirementPercentage(e.target.value)}
+                            step="1"
+                        />
+                        <span className="text-gray-700 mx-auto my-1">  {retirementPercentage}%</span>
 
-                            // Add other retirement method options here
-                        </select>
                     </label>
-                </div>
+                </label>
                 <Button type="submit">Calculate</Button>
             </form>
         </div>
     );
 };
 export default FirePage;
+
+
