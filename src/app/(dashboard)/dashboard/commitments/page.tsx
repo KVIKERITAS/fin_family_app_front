13a346f80b741d1903c090ff4480ae8086f0a4b9
LaurisV22 <100121715+LaurisV22@users.noreply.github.com>
2023-11-29 20:45:08 +0000

    sidebar update

---
+++ src/app/(dashboard)/dashboard/commitments/page.tsx
@@ -0,0 +1,12 @@
+const Finance = () => {
+  
+    return (
+        <div>
+    Commitments Page
+            here we will write down all financial commitments which will be transfered to personal-finance page
+        </div>
+    );
+};
+export default Finance;
+
+
