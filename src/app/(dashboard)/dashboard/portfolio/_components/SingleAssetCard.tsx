7e439f584f8da6ba64e371e05e6adc2ae694e9d4
Raminta Alisauskaite <raminta.alisauskaite@gmail.com>
2023-11-30 17:33:14 +0200

    asset cards created

--- src/app/(dashboard)/dashboard/portfolio/_components/SingleAssetCard.tsx
+++ src/app/(dashboard)/dashboard/portfolio/_components/SingleAssetCard.tsx
@@ -1,4 +1,5 @@
 import { TAsset } from '../page'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 type TAssetProp = {
 	asset: TAsset
@@ -6,9 +7,25 @@ type TAssetProp = {
 
 const SingleAsset = ({ asset }: TAssetProp) => {
 	return (
-		<div className='flex flex-col items-center mx-5 sm:mx-10 my-5'>
-			<asset.logo className='w-12 h-12 sm:w-20 sm:h-20' />
-			<p className='text-sm sm:text-lg my-2'>{asset.name}</p>
+		<div className='flex flex-col items-center gap-5'>
+			<Card className='w-44 h-72'>
+				<CardHeader>
+					<CardTitle>
+						<asset.logo className='w-12 h-12 sm:w-20 sm:h-20 m-auto' />
+						<p className='text-center text-sm sm:text-lg my-2'>{asset.name}</p>
+					</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<ul className='list-disc list-inside'>
+						{asset.description.map((listItem, index) => (
+							<li className='text-xs' key={index}>
+								{listItem}
+							</li>
+						))}
+					</ul>
+				</CardContent>
+			</Card>
+			<h4>{asset.value} EUR</h4>
 		</div>
 	)
 }
