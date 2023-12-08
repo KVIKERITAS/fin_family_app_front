6d5c029ce089cdb370b320fd31bcfd60753b6b18
spegisima <agneta.smergelyte@gmail.com>
2023-12-08 10:51:34 +0200

    delete modal improved and added into commitments page

--- src/app/(dashboard)/_components/DeleteModal.tsx
+++ src/app/(dashboard)/_components/DeleteModal.tsx
@@ -1,32 +1,33 @@
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from '@/components/ui/dialog'
-import { Button } from '@/components/ui/button'
-import { Trash2 } from 'lucide-react'
+import {Button} from '@/components/ui/button'
+import {Trash2} from 'lucide-react'
 
-const DeleteModal = () => {
-	return (
-		<Dialog>
-			<DialogTrigger className='px-2'><Trash2 /></DialogTrigger>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle className='mb-2'>Are you absolutely sure?</DialogTitle>
-					<DialogDescription>
-						This action cannot be undone. This will permanently delete your shit.
-						<span className='mt-4 flex gap-2 justify-center'>
-							<Button variant='outline'>Cancel</Button>
-							<Button>Delete</Button>
+const DeleteModal = ({ itemToDelete }: { itemToDelete: string }) => {
+    return (
+        <Dialog>
+            <DialogTrigger className='p-2'><Trash2/></DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle className='mb-2'>Are you sure you want to delete {itemToDelete}?</DialogTitle>
+                    <DialogDescription>
+                        This action cannot be undone. This will permanently delete your selected {itemToDelete}.
+                        <span className='mt-4 flex gap-2 justify-center'>
+                            <DialogClose><Button variant='outline'>Cancel</Button></DialogClose>
+							<Button className='bg-red-600 hover:bg-red-700'>Delete</Button>
 						</span>
-					</DialogDescription>
-				</DialogHeader>
-			</DialogContent>
-		</Dialog>
-	)
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
 }
 
 export default DeleteModal
